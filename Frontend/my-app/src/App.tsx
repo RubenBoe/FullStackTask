4df24124ef33bd59c0640cab0aae503f67b5cc02
Ruben <ruben.boesche@janus-consultants.de>
2023-01-22 15:00:57 +0100

    Initialized frontend app

---
+++ Frontend/my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+        <Typography variant='h1'>
+            Welcome to my App
+        </Typography>
+    </div>
+  );
+}
+
+export default App;
