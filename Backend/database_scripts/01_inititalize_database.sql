aa2476e70c5fc7d64da2ab884c7dad9b085e6342
Ruben <ruben.boesche@janus-consultants.de>
2023-01-22 11:21:30 +0100

    database setup

---
+++ Backend/database_scripts/01_inititalize_database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS task_fs;
+
+use task_fs;
+
+CREATE TABLE IF NOT EXISTS instancetable (
+	InstanceID int auto_increment PRIMARY KEY,
+	oID nvarchar(100),
+    instanceTimestamp bigint,
+	PersonID int,
+    PositionX decimal(5, 3),
+    PositionY decimal(5, 3)
+);
